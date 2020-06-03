672366d79694abb8de31ff70d7e5e7ddcc3c2ed6
Bryan <bryan.fennell@hotmail.com>
2020-06-02 22:54:04 -0400

    Fixed email link URIs.

--- src/components/DirectoryEntry.js
+++ src/components/DirectoryEntry.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Moment from 'react-moment'
 
 function DirectoryEntry({entry}) {
-  const thumbnailAlt = `${entry.name}'s photo`
+  const
+    thumbnailAlt = `${entry.name}'s photo`,
+    emailUri = `mailto:${entry.email}`
 
   return (
     <tr>
       <td><img src={entry.thumbnail} alt={thumbnailAlt} /></td>
       <td>{entry.name}</td>
       <td>{entry.phone}</td>
-      <td><a href="mailto:{entry.email}">{entry.email}</a></td>
+      <td><a href={emailUri}>{entry.email}</a></td>
       <td><Moment format="MM-DD-YYYY">{entry.dob}</Moment></td>
     </tr>
   )
